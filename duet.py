e0a8c4597183047460596ea8b67471a6f954eec6
Jeff Wu <wuthefwasthat@gmail.com>
2017-12-11 21:53:18 -0800

    finishing up

--- duet.py
+++ duet.py
@@ -21,12 +21,16 @@ class colors:
     GREEN = '\033[92m'
     RED = '\033[91m'
     END = '\033[0m'
+    GRAY = '\033[97m'
+
+def bold(str):
+    return colors.BOLD + str + colors.END
 
 def colored(str, color):
     return color + str + colors.END
 
 def strike(text):
-    return ''.join([u'\u0336{}'.format(c) for c in text])
+    return u'\u0336'.join([c for c in text])
 
 def colored_for_state(str, state):
     if state == -1:
@@ -37,7 +41,7 @@ def colored_for_state(str, state):
         assert state == 0
         return colored(str, colors.YELLOW)
 
-def print_grid(words, states, reverse=False):
+def print_grid(words, states, guesses_state, reverse=False):
     assert len(words) == 25
     print()
     for i in range(5):
@@ -49,19 +53,42 @@ def print_grid(words, states, reverse=False):
                 index = 5*i + j
             word = words[index]
             state = states[word]
-            str += colored_for_state('{:<20}'.format(words[index]), state)
+            padding = 20 - len(word)
+            prefix = '  '
+            if guesses_state[index] is not None:
+                (was_me, was_correct, round) = guesses_state[index]
+                if not was_me:
+                    prefix = u'\u25b2'
+                else:
+                    prefix = ' '
+                if was_correct:
+                    prefix += colored(u'\u2713', colors.GREEN)
+                    word = strike(word)
+                else:
+                    prefix += colored(u'\u2717', colors.RED)
+                    if was_me:
+                        word = strike(word)
+
+            str += prefix + ' ' + colored_for_state(word, state) + ' ' * padding
         print(str)
     print()
 
 def find_word(words, word):
-    return words.index(word.upper())
+    try:
+        return words.index(word)
+    except ValueError:
+        return -1
 
 def ask_word(question, words):
     while True:
         word = raw_input(question)
+        if word == '-':
+            return None
+        word = word.upper()
         index = find_word(words, word)
         if index >= 0:
-            return index
+            return word
+        print('That word is not on the board!')
 
 def other(player):
     assert player == 1 or player == 2
@@ -71,14 +98,14 @@ def setup(dictionary):
     words = sorted(random.sample(dictionary, 25))
 
     # states: 1 = good, 0 = bystander, -1 = assassin
-    p1state = {}
-    p2state = {}
+    p1board = {}
+    p2board = {}
 
     i = 0
     def add_state(num, p1, p2, i):
         for _ in range(num):
-            p1state[words[i]] = p1
-            p2state[words[i]] = p2
+            p1board[words[i]] = p1
+            p2board[words[i]] = p2
             i += 1
         assert i <= len(words)
         return i
@@ -105,7 +132,7 @@ def setup(dictionary):
 
     random.shuffle(words)
 
-    return words, p1state, p2state
+    return words, p1board, p2board
 
 def duet(args):
     player = args.player
@@ -114,35 +141,102 @@ def duet(args):
     random.seed(args.seed)
     with open(args.dictionary) as f:
         dictionary = map(lambda x: x.upper(), filter(len, f.read().split()))
-    words, p1state, p2state = setup(dictionary)
-    if args.player == 1:
-        print_grid(words, p1state)
-    else:
-        # could do reverse=True, but no need..
-        print_grid(words, p2state)
+    words, p1board, p2board = setup(dictionary)
 
     game_log = []
+
+    all_guesses = {} # dict from index to (player, correct, round)
+
+    def print_game():
+        guesses_state = []
+        for i in range(25):
+            if words[i] in all_guesses:
+                (p, correct, round) = all_guesses[words[i]]
+                guesses_state.append((p == player, correct, round))
+            else:
+                guesses_state.append(None)
+        if player == 1:
+            print_grid(words, p1board, guesses_state)
+        else:
+            # could do reverse=True, but no need..
+            print_grid(words, p2board, guesses_state)
+
     round = 1
     cluer = 1
+    ncorrect = 0
+    lost = False
+
+    def write_log():
+        if args.logfile:
+            with open(args.logfile, 'w') as f:
+                f.write(json.dumps({
+                    'won': not lost,
+                    'board': [p1board, p2board],
+                    'log': game_log
+                }, indent=2))
+
     while round <= 9:
+        print()
+        print('-- ROUND 1 --')
+        print()
+        print_game()
+
+        if cluer == 1:
+            state = p1board
+        else:
+            state = p2board
 
         if cluer == player:
-            clue = raw_input('Give a clue? ')
+            clue = raw_input('Give a clue: ')
         else:
             clue = raw_input('What did they clue? ')
+
+        round_guesses = []
+        while True:
+            if cluer == player:
+                guessed = ask_word('What did they guess (Dash if nothing)? ', words)
+                who = 'They'
+            else:
+                guessed = ask_word('Guess a word (Dash to stop): ', words)
+                who = 'You'
+            if guessed is None:
+                break
+            round_guesses.append(guessed)
+            if state[guessed] == -1:
+                print(colored('YOU LOSE (guessed an assassin)!', colors.RED))
+                lost = True
+                break
+            elif state[guessed] == 0:
+                print(colored('%s guessed a bystander!' % who, colors.YELLOW))
+                all_guesses[guessed] = (other(cluer), False, round)
+                break
+            else:
+                print(colored('%s recovered an agent!  %s may guess again.' % (who, who), colors.GREEN))
+                all_guesses[guessed] = (other(cluer), True, round)
+                ncorrect += 1
+                print_game()
+                if ncorrect == 15: # won!
+                    break
 
         game_log.append({
             'round': round,
             'clue': clue,
+            'guesses': round_guesses,
+            'ncorrect': ncorrect,
         })
+        if lost:
+            return write_log()
+
+        if ncorrect == 15:
+            print(colored('YOU WIN!', colors.GREEN))
+            return write_log()
+
         round += 1
         cluer = other(cluer)
 
-    index = ask_word('Guess a word? ', words)
-
-    if args.logfile:
-        with open(args.logfile, 'w') as f:
-            f.write(json.dumps(game_log, indent=2))
+    lost = True
+    print(colored('YOU LOSE (no more turns)!', colors.GREEN))
+    return write_log()
 
 if __name__ == "__main__":
     args = parser.parse_args()
